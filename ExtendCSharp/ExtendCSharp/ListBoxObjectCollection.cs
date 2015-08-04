12fd67a0b2797883233ba7b69a0db238ed792fb3
Luca Pozzi <lpozzi44@gmail.com>
2015-08-04 10:22:34 +0200

    Creazione Extension Library

---
+++ ExtendCSharp/ExtendCSharp/ListBoxObjectCollection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExtendCSharp
+{
+    public static class ListBoxObjectCollection
+    {
+        public static void AddUnique(this ListBox.ObjectCollection self, object obj)
+        {
+            foreach (object o in self)
+            {
+                if (o.Equals(obj))
+                    return;
+            }
+            self.Add(obj);
+        }
+    }
+}
