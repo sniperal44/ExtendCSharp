709f1278b99b04cf08843acf1e15d32d39579b61
Luca Pozzi <lpozzi44@gmail.com>
2015-08-24 00:07:28 +0200

    Aggiunto Ricerca tutte occorrenze di una stringa

---
+++ ExtendCSharp/ExtendCSharp/Extension.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExtendCSharp
+{
+    public static class Extension
+    {        
+        #region NUMBER
+        public static bool IsInteger(this double d)
+        {
+            return (d - (int)d) == 0;
+        }
+        public static double Sqrt(this double d)
+        {
+            return Math.Sqrt(d);
+        }
+        public static double Sqrt(this int d)
+        {
+            return Math.Sqrt(d);
+        }
+        #endregion
+
+        #region String
+        public static bool IsInt(this String d)
+        {
+            int n;
+            return int.TryParse(d, out n);
+        }
+        public static int ParseInt(this String d)
+        {
+            return int.Parse(d);
+        }
+        public static List<int> AllIndexesOf(this String str, string value)
+        {
+            List<int> indexes = new List<int>();
+            if (String.IsNullOrEmpty(value))
+                return indexes;
+            for (int index = 0; ; index += value.Length)
+            {
+                index = str.IndexOf(value, index);
+                if (index == -1)
+                    return indexes;
+                indexes.Add(index);
+            }
+        }
+        #endregion
+
+        #region Control
+        public static void SetTextInvoke(this Control t, string s)
+        {
+            if (t.InvokeRequired)
+                t.Invoke((MethodInvoker)delegate { t.SetTextInvoke(s); });
+            else
+                t.Text = s;
+        }
+        public static void SetEnableInvoke(this Control t, bool b)
+        {
+            if (t.InvokeRequired)
+                t.Invoke((MethodInvoker)delegate { t.SetEnableInvoke(b); });
+            else
+                t.Enabled = b;
+        }
+
+        public static Control SetSize(this Control t, int Width, int Height)
+        {
+            t.Size = new System.Drawing.Size(Width, Height);
+            return t;
+        }
+        #endregion
+
+        #region Control.ControlCollection
+        public static void AddVertical(this Control.ControlCollection self, Control c)
+        {
+            int y = 0;
+            foreach (Control cc in self)
+            {
+                if (cc.Location.Y + cc.Height > y)
+                    y = cc.Location.Y + cc.Height;
+            }
+            c.Location = new System.Drawing.Point(0, y);
+            self.Add(c);
+        }
+        #endregion
+
+        #region FileInfo
+        public static string GetFileNameWithoutExtension(this FileInfo self)
+        {
+            return Path.GetFileNameWithoutExtension(self.Name);
+        }
+        #endregion
+
+        #region ProgressBar
+        public static void SetValueInvoke(this ProgressBar p, int Value)
+        {
+            if (p.InvokeRequired)
+                p.Invoke((MethodInvoker)delegate { p.SetValueInvoke(Value); });
+            else
+                p.Value = Value;
+        }
+        #endregion
+
+        #region ListBox.ObjectCollection
+        public static void AddUnique(this ListBox.ObjectCollection self, object obj)
+        {
+            foreach (object o in self)
+            {
+                if (o.Equals(obj))
+                    return;
+            }
+            self.Add(obj);
+        }
+        #endregion
+
+        #region object
+        public static T Cast<T>(this object o)
+        {
+            return (T)o;
+        }
+        #endregion
+
+
+
+
+
+
+
+        #region DEMO
+
+        #endregion
+    }
+}
---
+++ ExtendCSharp/ExtendCSharp/TimeSpanPlus.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtendCSharp
+{
+    public class TimeSpanPlus
+    {
+        private TimeSpan _time;
+        public TimeSpan Time
+        {
+            get
+            {
+                return _time;
+            }
+            set
+            {
+                _time = value;
+            }
+        }
+        public TimeSpanPlus(long ticks)
+        {
+            _time = new TimeSpan(ticks);
+        }
+        public TimeSpanPlus(int milliseconds = 0, int seconds = 0, int minutes = 0, int hours = 0, int days = 0)
+        {
+            _time = new TimeSpan(days, hours, minutes, seconds, milliseconds);
+        }
+
+
+        public void AddSeconds(int seconds)
+        {
+            _time = _time.Add(new TimeSpan(0, 0, seconds));
+        }
+        public void AddMinuts(int minutes)
+        {
+            _time = _time.Add(new TimeSpan(0, minutes, 0));
+        }
+        public void AddHours(int hours)
+        {
+            _time = _time.Add(new TimeSpan(hours, 0, 0));
+        }
+        public void AddTime(int sec, int min, int hou)
+        {
+            _time = _time.Add(new TimeSpan(hou, min, sec));
+        }
+        override public string ToString()
+        {
+            return _time.ToString();
+        }
+        public string ToString(string format)
+        {
+            return _time.ToString(format);
+        }
+
+        public void SetTime(int milliseconds = 0, int seconds = 0, int minutes = 0, int hours = 0, int days = 0)
+        {
+            _time = new TimeSpan(days, hours, minutes, seconds, milliseconds);
+        }
+    }
+}
