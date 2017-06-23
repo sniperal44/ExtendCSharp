5530eccd1fee6265820a4a8e508a3ca46d75b5eb
Luca ( Alpa ) <lpozzi44@gmail.com>
2017-06-23 10:53:59 +0200

    - Aggiunta ButtonMenu

---
+++ ExtendCSharp/ExtendCSharp/Controls/ButtonMenu.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace ExtendCSharp.Controls
+{
+    partial class ButtonMenu
+    {
+        /// <summary> 
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione componenti
+
+        /// <summary> 
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare 
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button = new System.Windows.Forms.Button();
+            this.button_arrow = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button
+            // 
+            this.button.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button.Location = new System.Drawing.Point(0, 0);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(72, 48);
+            this.button.TabIndex = 0;
+            this.button.Text = "button";
+            this.button.UseVisualStyleBackColor = true;
+            // 
+            // button_arrow
+            // 
+            this.button_arrow.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_arrow.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_arrow.Location = new System.Drawing.Point(71, 0);
+            this.button_arrow.Name = "button_arrow";
+            this.button_arrow.Size = new System.Drawing.Size(21, 48);
+            this.button_arrow.TabIndex = 1;
+            this.button_arrow.Text = "▼";
+            this.button_arrow.UseVisualStyleBackColor = true;
+            this.button_arrow.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // ButtonMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button_arrow);
+            this.Controls.Add(this.button);
+            this.Name = "ButtonMenu";
+            this.Size = new System.Drawing.Size(92, 48);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button_arrow;
+        public System.Windows.Forms.Button button;
+    }
+}
---
+++ ExtendCSharp/ExtendCSharp/Controls/ButtonMenu.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExtendCSharp.Controls
+{
+    public partial class ButtonMenu : UserControl
+    {
+        //TODO: vedo se è possibile modificare un control di un altro control ( modificare il bottone dalla view del MainForm ) 
+        String DownArrow = "▼";
+        String UpArrow = "▲";
+
+        ContextMenuStripStatus MenuContextStatus = ContextMenuStripStatus.Close;
+
+
+        ContextMenuStrip _contextMenuStrip = null;
+        bool ClosedLunchedByButton = false;
+
+        public ContextMenuStrip MenuContext {
+            get
+            {
+                return _contextMenuStrip;
+            }
+            set
+            {
+                _contextMenuStrip = value;
+                if (_contextMenuStrip != null)
+                {
+                    _contextMenuStrip.Opened += ContextMenuStrip_Opened;
+                    _contextMenuStrip.Closed += ContextMenuStrip_Closed;
+                    _contextMenuStrip.Closing += _contextMenuStrip_Closing;
+                }
+            }
+
+        }
+
+        private void _contextMenuStrip_Closing(object sender, ToolStripDropDownClosingEventArgs e)
+        {
+            if (!ClosedLunchedByButton && button_arrow.ClientRectangle.Contains(button_arrow.PointToClient(Control.MousePosition)))
+            {
+                e.Cancel = true;
+            }
+            ClosedLunchedByButton = false;
+
+        }
+
+        public ButtonMenu()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        
+
+        private void ContextMenuStrip_Opened(object sender, EventArgs e)
+        {
+            button_arrow.Text = UpArrow;
+            MenuContextStatus = ContextMenuStripStatus.Open;
+        }
+        private void ContextMenuStrip_Closed(object sender, ToolStripDropDownClosedEventArgs e)
+        {
+            button_arrow.Text = DownArrow;
+            MenuContextStatus = ContextMenuStripStatus.Close;
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (_contextMenuStrip != null && _contextMenuStrip.Items.Count!=0)
+            {
+                if(MenuContextStatus==ContextMenuStripStatus.Close)
+                {
+                    _contextMenuStrip.Show(button.PointToScreen( button.ClientRectangle.GetLocation(ContentAlignment.BottomLeft)));
+                }
+                else if (MenuContextStatus == ContextMenuStripStatus.Open)
+                {
+                    ClosedLunchedByButton = true;
+                    _contextMenuStrip.Close();
+                }
+                
+            }
+            
+        }
+    }
+    enum ContextMenuStripStatus
+    {
+        Open,
+        Close
+    }
+}
