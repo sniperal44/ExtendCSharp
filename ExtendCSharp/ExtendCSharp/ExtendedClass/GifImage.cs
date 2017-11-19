53846dd4d78f86ba999b8d4c27796dba74103151
Luca <lpozzi44@gmail.com>
2017-11-19 14:58:42 +0100

    - class GifImage -> permette di ricevere un immagine GIF come input e gestire i frame e la velocità di visualizzazione della gif - class ResourceService -> permette di gestire le risorse incorporate all'eseguibile accedendo allo Stream              per recuperare un Oggetto dallo stream occorre passare per un ResourceParse              Creata classe astratta ResourceParse e implementati ResourceParser  di Image e String

---
+++ ExtendCSharp/ExtendCSharp/ExtendedClass/GifImage.cs
@@ -0,0 +1,187 @@
+﻿using ExtendCSharp.Properties;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtendCSharp.ExtendedClass
+{
+    public class GifImage
+    {
+        //Image[] Frames;
+        int FramesCount = 0;
+        int delay = 0;
+        Image img = null;
+
+        Stopwatch watch = null;
+        int CurrentFrame = 0;
+
+        int _Width;
+        public int Width
+        {
+            get => _Width;
+        }
+
+        int _Height;
+        public int Height
+        {
+            get => _Height;
+        }
+
+        bool _Revers;
+        public bool Revers
+        {
+            get => _Revers;
+            set
+            {
+                _Revers = value;
+            }
+        }
+
+        int totalTime = 0;
+        Dictionary<int, int> listDelay = null;
+        
+
+        FrameDimension _dimension;
+        public GifImage(Image i)
+        {
+            Init( i); 
+        }
+        public GifImage(String Path)
+        {
+            Init(Image.FromFile(Path));
+        }
+
+        private void Init(Image img)
+        {
+            this.img = img;
+            _Width = img.Width;
+            _Height = img.Height;
+
+            _dimension = new FrameDimension(img.FrameDimensionsList[0]);
+            FramesCount = img.GetFrameCount(_dimension);
+
+            byte[] times = img.GetPropertyItem(0x5100).Value;
+            delay = BitConverter.ToInt32(times, 4 * 0)*10;  //TODO: implementare velocità variabile
+            watch = new Stopwatch();
+            listDelay = new Dictionary<int, int>();
+
+            int currentTime = 0;                        //trovo la durata totale della gif e memorizzo tutte le durate
+            for (var i = 0; i < FramesCount; i++)
+            {
+                currentTime = BitConverter.ToInt32(times, i * 4);
+                listDelay.Add(totalTime, currentTime);
+                totalTime += currentTime;
+            }
+            totalTime *= 10;
+
+
+
+        }
+
+
+        public bool IsRunning
+        {
+            get
+            {
+                return watch.IsRunning;
+            }
+        }
+
+
+        public void Start()
+        {
+            watch.Start();
+        }
+        public void Restart()
+        {
+            CurrentFrame = 0;
+            watch.Restart();
+        }
+        public void Stop()
+        {
+            watch.Stop();
+        }
+
+
+
+        public Image GetImage()
+        {
+            if (watch.IsRunning)
+            {
+                int OldFrame = CurrentFrame;
+                int FrameElapsed = (int)(watch.ElapsedMilliseconds / delay);
+                if(_Revers)
+                {
+                    CurrentFrame = CurrentFrame - (FrameElapsed % FramesCount);
+                    if (CurrentFrame < 0)
+                        CurrentFrame += FramesCount;
+                }
+                else
+                {
+                    CurrentFrame = (CurrentFrame + FrameElapsed) % FramesCount;
+                }
+               
+               
+                if (OldFrame != CurrentFrame)
+                {
+                    watch.Restart();
+                    img.SelectActiveFrame(_dimension, CurrentFrame);
+                }
+            }
+
+            return img;
+        }
+
+
+        //TODO: penso ad un idea migliore per gestire i delay per ogni singolo frame
+        private int FindDelayIndex(int CurrentTime)
+        {
+            int[] keys = listDelay.Keys.ToArray();
+            //TODO :penso se implementare la binary search
+            /*if (keys.Length == 0)
+                return -1;
+            else if( keys.Length==1)
+            {
+                return keys[0];
+            }
+            else
+            {
+                int i = 0, f = keys.Length - 1;
+                //TODO: FINISCO BINARY SEARCH
+                while (true)
+                {
+                    int m = (i + f) / 2;
+                    if (CurrentTime>keys[m])
+                        i = m;
+                    else
+                        f = m;
+                }
+            }*/
+
+
+            if (keys.Length == 0)
+                return -1;
+            else if (keys.Length == 1)
+            {
+                return keys[0];
+            }
+            else
+            {
+                int l = keys.Length;
+                for (int i = 0; i < l; i++)
+                {
+                    if (CurrentTime < keys[i] )
+                        return i==0?-1:keys[i - 1];
+                }
+            }
+            return -1;
+        }
+
+    }
+}
---
+++ ExtendCSharp/ExtendCSharp/Services/ResourcesService.cs
@@ -0,0 +1,138 @@
+﻿using ExtendCSharp.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtendCSharp.Services
+{
+    public class ResourcesService: IService
+    {
+        private Assembly targetAssembly;
+
+        private Dictionary<Type, ResourceParser> parsers;
+
+        public ResourcesService(Assembly targetAssembly)
+        {
+            this.targetAssembly = targetAssembly;
+
+
+            //Inizializa parser
+            parsers = new Dictionary<Type, ResourceParser>();
+            RegisterParser<String>(new ResourceStringParse());
+            RegisterParser<Image>(new ResourceImageParse());
+
+
+        }
+        /// <summary>
+        /// Ritorna uno stream ad una RISORSA INCORPORATA ( proprietà della risorsa -> "Azione di compilazione" = Risorsa incorporata )
+        /// </summary>
+        /// <param name="assembly">assembly dove recuperare la risorsa 
+        /// <para /> System.Reflection.Assembly.GetExecutingAssembly() -> assembly corrente
+        /// <para />System.Reflection.Assembly.GetEntryAssembly() -> primo assembly eseguito
+        /// </param>
+        /// <param name="ResourcePath">Path della RISORSA INCORPORATA nello stream.
+        /// <para /> 
+        /// <para />Esempio:
+        /// <para />extendCSharpTest.Gif.test.gif
+        /// <para />extendCSharpTest = Nome del progetto ( assembly di esecuzione ) 
+        /// <para />Gif = sottocartella ( aggiungere altre sottocartelle separate dal . ) 
+        /// <para />test.gif = nome del file
+        /// </param>
+        /// <returns></returns>
+        public Stream GetStream(String ResourcePath)
+        {
+            return targetAssembly.GetManifestResourceStream(ResourcePath);
+        }
+
+
+        public T GetObject<T>( String ResourcePath)
+        {
+            ResourceParser rp = GetParser<T>();
+            if( rp==null)
+            {
+                throw new NotImplementedException("Parser corrispondente non implementato");
+            }
+            else
+            {
+                T temp;
+                using (Stream s = GetStream(ResourcePath))
+                {
+                    temp= rp.Parse(s)._Cast<T>();
+                }
+                return temp;
+            }
+        }
+
+
+        public void RegisterParser<T>(ResourceParser parser)
+        {
+            parsers.Add(parser.GetType(), parser);
+        }
+        private ResourceParser GetParser<T>()
+        {
+            if (parsers.ContainsKey(typeof(T)))
+                return parsers[typeof(T)];
+            return null;
+        }
+    }
+
+
+
+    public abstract class ResourceParser
+    {
+        Type resourceType;
+        protected ResourceParser(Type resourceType)
+        {
+            this.resourceType = resourceType;
+        }
+        public abstract object Parse(Stream s);
+
+        public new Type GetType()
+        {
+            return resourceType;
+        }
+    }
+
+    public class ResourceStringParse : ResourceParser
+    {
+        public ResourceStringParse():base(typeof(String))
+        {
+
+        }
+        protected ResourceStringParse(Type resourceType) : base(resourceType)
+        {
+        }
+
+        public override object Parse(Stream s)
+        {
+            string text;
+            using (StreamReader reader = new StreamReader(s))
+            {
+                text = reader.ReadToEnd();
+            }
+            return text;
+        }
+    }
+    public class ResourceImageParse : ResourceParser
+    {
+        public ResourceImageParse():base(typeof(Image))
+        {
+
+        }
+        protected ResourceImageParse(Type resourceType) : base(resourceType)
+        {
+        }
+
+        public override object Parse(Stream s)
+        {
+            return Image.FromStream(s);  
+        }
+    }
+
+
+}
