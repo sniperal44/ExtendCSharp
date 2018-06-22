b38f61bfcf952fa60c8c71433a586bf2bb456457
Luca ( Alpa ) <lpozzi44@gmail.com>
2018-06-22 11:29:54 +0200

    MySQLFieldAttribute -> attributo per la gestione dei campi MySQL _TestService -> testing della lettura dei field/property tramite Attributo data una classe da template

--- ExtendCSharp/ExtendCSharp/Services/_TestService.cs
+++ ExtendCSharp/ExtendCSharp/Services/_TestService.cs
@@ -1,4 +1,5 @@
-﻿using ExtendCSharp.Interfaces;
+﻿using ExtendCSharp.Attributes;
+using ExtendCSharp.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,14 +12,34 @@ namespace ExtendCSharp.Services
     public class _TestService : IService
     {
 
-        public void Test(object obj)
+        public T Test<T>() where T : new()
         {
-            
-            
-            foreach (PropertyInfo propertyInfo in obj.GetType().GetProperties())
+
+            Type tipo = typeof(T);
+            //PropertyInfo[] pi = tipo.GetProperties();
+            //FieldInfo[] fi=tipo.GetFields();
+            T newObj = new T();
+
+
+
+            FieldInfo[] Campi = tipo.GetFields().Where(prop => Attribute.IsDefined(prop, typeof(MySQLFieldAttribute))).ToArray();
+            FieldInfo CampoSpecifico= Campi.First(field=> Attribute.GetCustomAttribute(field, typeof(MySQLFieldAttribute))._Cast<MySQLFieldAttribute>().Name == "NOME");
+
+
+
+
+            /*foreach (PropertyInfo propertyInfo in pi)
             {
                 Log.Log.AddLog(propertyInfo.ToString());
             }
+
+            foreach (FieldInfo fieldInfo in fi)
+            {
+                fieldInfo.SetValue(newObj, 1);
+            }*/
+
+
+            return newObj;
         }
     }
 }
