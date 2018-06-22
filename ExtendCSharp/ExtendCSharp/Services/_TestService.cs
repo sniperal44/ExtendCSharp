f210336612a173329aff8183a641923c1fd92f43
Luca ( Alpa ) <lpozzi44@gmail.com>
2018-06-22 10:33:52 +0200

    Aggiunto _TestService -> verrà usato per testare le funzionalità di nuovi servizi

---
+++ ExtendCSharp/ExtendCSharp/Services/_TestService.cs
@@ -0,0 +1,24 @@
+﻿using ExtendCSharp.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtendCSharp.Services
+{
+    public class _TestService : IService
+    {
+
+        public void Test(object obj)
+        {
+            
+            
+            foreach (PropertyInfo propertyInfo in obj.GetType().GetProperties())
+            {
+                Log.Log.AddLog(propertyInfo.ToString());
+            }
+        }
+    }
+}
