8312478a140aaa3bc2c1761d6c5664a16c7fe527
Luca ( Alpa ) <lpozzi44@gmail.com>
2018-07-02 10:12:01 +0200

    - Aggiunte direttive al CSPROJ per definire una direttiva di copilazione per la versione del framework ( esempio: NETFX4_5 ) - TestService -> testate le direttive del compilatore in base alla versione del netframework

--- ExtendCSharp/ExtendCSharp/Services/_TestService.cs
+++ ExtendCSharp/ExtendCSharp/Services/_TestService.cs
@@ -5,59 +5,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
-using System.Threading.Tasks;
+
 
 namespace ExtendCSharp.Services
 {
     public class _TestService : IService
     {
 
-        public IEnumerable<T> Test<T>() where T : new()
+        public void Test()
         {
 
-            Type tipo = typeof(T);
-            List<T> OutList = new List<T>();
-            Dictionary<String, FieldInfo> fieldDictionary = new Dictionary<string, FieldInfo>();
-            
 
-          
-            //Recupero tutti i campi PUBBLICI con l'attributo MySQLFieldAttribute
-            FieldInfo[] Campi = tipo.GetFields().Where(prop => Attribute.IsDefined(prop, typeof(MySQLFieldAttribute))).ToArray();
-            //Se si vogliono trovare tutte le Proprietà: sostituire GetFields con GetProperties
-            
-            
+#if (NETFX4_5)
+            Log.Log.AddLog("v4_5 was set");
+#endif
 
+#if (NETFX4_0)
+        Log.Log.AddLog("v4_0 was set");
+#endif
+#if (NETFX3_5)
 
-            //Recupero un campo dato il Name specificato nel MySQLFieldAttribute -> ritorna NULL se non lo trova
-            FieldInfo CampoSpecifico = Campi.FirstOrDefault(field=> Attribute.GetCustomAttribute(field, typeof(MySQLFieldAttribute))._Cast<MySQLFieldAttribute>().Name == "NOME");
+        Log.Log.AddLog("v3_5 was set");
+
+#endif
 
 
-
-            //recupero campo per campo in base al nome e li inserisco in un dizionario per una ricerca più rapida
-            FieldInfo Intero = Campi.FirstOrDefault(field => Attribute.GetCustomAttribute(field, typeof(MySQLFieldAttribute))._Cast<MySQLFieldAttribute>().Name == "Intero");
-            fieldDictionary.Add("Intero", Intero);
-
-            FieldInfo Decimale = Campi.FirstOrDefault(field => Attribute.GetCustomAttribute(field, typeof(MySQLFieldAttribute))._Cast<MySQLFieldAttribute>().Name == "Decimale");
-            fieldDictionary.Add("Decimale", Decimale);
-
-            FieldInfo Stringa = Campi.FirstOrDefault(field => Attribute.GetCustomAttribute(field, typeof(MySQLFieldAttribute))._Cast<MySQLFieldAttribute>().Name == "Stringa");
-            fieldDictionary.Add("Stringa", Stringa);
-
-
-
-            //ciclo di tutti gli elementi
-            for( int i=0;i<10;i++)
-            {
-                T tmp = new T();
-                fieldDictionary["Intero"]?.SetValue(tmp,null);
-                fieldDictionary["Decimale"]?.SetValue(tmp, null);
-                fieldDictionary["Stringa"]?.SetValue(tmp, "Hello World!");
-                OutList.Add(tmp);
-            }
-
-          
-
-            return OutList;
         }
     }
 }
