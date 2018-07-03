941744113422b165d3a5980f9ce1d01c68279680
Luca <lpozzi44@gmail.com>
2018-07-03 12:16:46 +0200

    - Creata sezione per Framework 4.0.3

---
+++ ExtendCSharp/ExtendCSharp/_v4_0_Fix/Tuple_3_5.cs
@@ -0,0 +1,29 @@
+﻿
+
+#if (NETFX3_5)
+namespace ExtendCSharp._v3_5_Fix
+{
+
+    public class Tuple<T1, T2>
+    {
+        public T1 Item1 { get; private set; }
+        public T2 Item2 { get; private set; }
+        internal Tuple(T1 Item1, T2 Item2)
+        {
+            this.Item1 = Item1;
+            this.Item2 = Item2;
+        }
+    }
+    public static class Tuple
+    {
+        public static Tuple<T1, T2> New<T1, T2>(T1 Item1, T2 Item2)
+        {
+            var tuple = new Tuple<T1, T2>(Item1, Item2);
+            return tuple;
+        }
+    }
+}
+#endif
+
+
+
