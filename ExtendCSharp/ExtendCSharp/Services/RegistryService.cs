549c48e9e66f384a561eca366d066b0701de66dc
Luca Pozzi <lpozzi44@gmail.com>
2018-07-14 14:16:27 +0200

    RegisterService -> RegistryService

---
+++ ExtendCSharp/ExtendCSharp/Services/RegistryService.cs
@@ -0,0 +1,31 @@
+﻿using ExtendCSharp.Interfaces;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtendCSharp.Services
+{
+    //TODO:
+    public class RegistryService:IService
+    {
+        public RegistryService()
+        {
+
+        }
+
+        
+
+        public object GetValue(RegistryKey rkey, String ValueName)
+        {
+            return rkey.GetValue(ValueName);
+        }
+        public object GetValue(String rkey,String ValueName,object _default=null)
+        {
+            return Registry.GetValue(rkey, ValueName, _default);
+        }
+
+    }
+}
