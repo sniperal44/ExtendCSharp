11b6a228631aa52a15b82dee4ee76ddeee072503
Luca <lpozzi44@gmail.com>
2019-02-04 22:26:29 +0100

    - Aggiunto ResourceParser -> BitmapImage ( WPF )

---
+++ ExtendCSharp/ExtendCSharp/WPF/ExtendedClass/ResourceParserWPF.cs
@@ -0,0 +1,31 @@
+﻿using ExtendCSharp.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace ExtendCSharp.WPF.ExtendedClass
+{
+    public class ResourceBitmapImageParse : ResourceParser
+    {
+        public ResourceBitmapImageParse() : base(typeof(BitmapImage))
+        {
+
+        }
+        protected ResourceBitmapImageParse(Type resourceType) : base(resourceType)
+        {
+        }
+
+        public override object Parse(Stream s)
+        {
+            var bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.StreamSource = s;
+            bitmapImage.EndInit();
+            return bitmapImage;
+        }
+    }
+}
