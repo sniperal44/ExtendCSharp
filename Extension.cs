27b4705659a5b370c274577ccf1961d013d99743
Luca <lpozzi44@gmail.com>
2019-02-09 11:26:34 +0100

    Extension -> ExifRotate -> applica la rotazione di un image in base al metadato EXIF

--- Extension.cs
+++ Extension.cs
@@ -1702,6 +1702,7 @@ namespace ExtendCSharp
         #endregion
 
         #region Image
+        private const int exifOrientationID = 0x112; //274
         public static Image CloneFast(this Image img)
         {
             int Width = img.Width;
@@ -1712,6 +1713,33 @@ namespace ExtendCSharp
                 g.DrawImage(img, 0, 0, Width, Height);
             }
             return bmp;
+        }
+        
+        /// <summary>
+        /// Ruota un immagine in base al suo valore EXIF ( metadata ) di orientamento
+        /// </summary>
+        /// <param name="img"></param>
+        public static void ExifRotate(this Image img)
+        {
+            if (!img.PropertyIdList.Contains(exifOrientationID))
+                return;
+
+            var prop = img.GetPropertyItem(exifOrientationID);
+            int val = BitConverter.ToUInt16(prop.Value, 0);
+            var rot = RotateFlipType.RotateNoneFlipNone;
+
+            if (val == 3 || val == 4)
+                rot = RotateFlipType.Rotate180FlipNone;
+            else if (val == 5 || val == 6)
+                rot = RotateFlipType.Rotate90FlipNone;
+            else if (val == 7 || val == 8)
+                rot = RotateFlipType.Rotate270FlipNone;
+
+            if (val == 2 || val == 4 || val == 5 || val == 7)
+                rot |= RotateFlipType.RotateNoneFlipX;
+
+            if (rot != RotateFlipType.RotateNoneFlipNone)
+                img.RotateFlip(rot);
         }
 
         #endregion
