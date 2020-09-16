09956ff9486bd0e3af9a20c9c0e3626c544ef40b
Luca <lpozzi44@gmail.com>
2020-09-16 16:06:51 +0200

    Aggiunta classe manager per Excel

---
+++ Excel/ExcelCoordinates.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtendCSharp
+{
+    public class ExcelCoordinates : ICloneable
+    {
+
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+
+
+        public ExcelCoordinates(int column, int row)
+        {
+            Row = row;
+            Column = column;
+        }
+
+        public ExcelCoordinates(string column, int row)
+        {
+            Row = row;
+            Column = ConvertColumntStringIntoNumber(column);
+        }
+
+
+        public ExcelCoordinates(string ColumnRow)
+        {
+            ColumnRow = ColumnRow.ToUpper();
+            int Status = 0; //0 = controllo lettere | 1 = controllo numeri
+            String ColumnS = "";
+            String RowS = "";
+
+            for (int i = 0; i < ColumnRow.Length; i++)
+            {
+                if (Status == 0)      //se mi aspetto un carattere
+                {
+                    if (ColumnRow[i].IsUpperChar())     //e trovo un carattere
+                    {
+                        ColumnS += ColumnRow[i];     //lo aggiungo alle colonne
+                    }
+                    else if (ColumnRow[i].IsNumber())    //se invece trovo un numero 
+                    {
+                        if (ColumnS.Length == 0)         //se non ho ancora trovato una lettera
+                        {
+                            throw new ArgumentException("Attesa lettera");         //sintassi errata
+                        }
+                        else   //avendo invece già trovato una lettera, vuol dire che posso passare alla parte numeri
+                        {
+                            Status = 1;
+                            RowS += ColumnRow[i];
+                        }
+                    }
+                    else //se trovo altro
+                    {
+                        throw new ArgumentException("Attesa lettera o numero");
+                    }
+
+                }
+                else if (Status == 1)
+                {
+                    if (ColumnRow[i].IsNumber())
+                    {
+                        RowS += ColumnRow[i];
+                    }
+                    else   //qualsiasi altro carattere diverso da un numero ( in questa fase ) è un errore
+                    {
+                        throw new ArgumentException("Atteso numero");
+                    }
+
+                }
+            }
+            Row = RowS.ParseInt();
+            Column = ConvertColumntStringIntoNumber(ColumnS);
+        }
+
+
+        private static int ConvertColumntStringIntoNumber(string ColumnsString)
+        {
+            ColumnsString = ColumnsString.ToUpper();
+            int N = 0;
+            ColumnsString = ColumnsString.ReverseString();
+
+            int NumeroLettereAlfabeto = 26;
+            for (int i = 0; i < ColumnsString.Length; i++)
+            {
+                //i corrisponde alla potenza 
+                char c = ColumnsString[i];
+                if (!(c >= 'A' && c <= 'Z'))
+                    throw new ArgumentException("Lettera non valida: " + c + "\r\nStringa di riferimento: " + ColumnsString);
+
+                int pos = (c - 'A') + 1;
+                N += pos * (int)Math.Pow(NumeroLettereAlfabeto, i);
+            }
+            return N;
+        }
+
+        public static String ConvertColumnRowToString(int Col,int Row)
+        {
+            String Lettere="";
+            Col--;
+            int NumeroLettereAlfabeto = 26;
+            do
+            {
+                Lettere += (char)((Col % NumeroLettereAlfabeto)+(int)'A');
+                Col /= NumeroLettereAlfabeto;
+            } while (Col>0);
+
+            return Lettere + Row;
+        }
+        public String ToStringRef()
+        {
+            return ConvertColumnRowToString(Column, Row);
+        }
+        public object Clone()
+        {
+            return new ExcelCoordinates(Column, Row);
+        }
+
+        public ExcelCoordinates CastClone()
+        {
+            return (ExcelCoordinates)Clone();
+        }
+    }
+}
---
+++ Excel/ExcelManager.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtendCSharp.Excel
+{
+    public class ExcelManager
+    {
+        public event AppEvents_WorkbookBeforeCloseEventHandler WorkbookBeforeClose;
+        //TODO: implemento gli altri eventi
+
+        protected String FileUri;
+        protected Microsoft.Office.Interop.Excel.Application XApplication;
+        protected Microsoft.Office.Interop.Excel._Workbook XWorkbook;
+        //protected Microsoft.Office.Interop.Excel._Worksheet XSheet;
+        //protected Microsoft.Office.Interop.Excel.Range XRng;
+
+        public ExcelManager(String uri)
+        {
+            this.FileUri = uri;
+            OpenFile(uri);
+        }
+
+        private void OpenFile(string FileName)
+        {
+            XApplication = new Microsoft.Office.Interop.Excel.Application();
+            XWorkbook = XApplication.Workbooks.Open(FileName);
+
+            XApplication.Visible = true;
+            XApplication.WorkbookBeforeClose += (Workbook Wb, ref bool Cancel)=> { WorkbookBeforeClose?.Invoke(Wb,ref Cancel); };
+        }
+
+
+
+        public List<String> GetSheetsName()
+        {
+            List<String> sheetNames = new List<string>();
+            foreach (Worksheet worksheet in XWorkbook.Worksheets)
+            {
+                sheetNames.Add(worksheet.Name);
+            }
+
+
+            return sheetNames;
+
+        }
+
+        public Worksheet GetWorksheets(String Name)
+        {
+            return (Worksheet)XWorkbook.Worksheets[Name];
+        }
+
+        public Worksheet CreateWorksheet(String Name)
+        {
+            if (!WorksheetExist(Name))
+            {
+                Worksheet ws = (Worksheet)XWorkbook.Worksheets.Add(Type.Missing, XWorkbook.Worksheets.GetEnumerable().Last(), Type.Missing, Type.Missing);
+                ws.Name = Name;
+                return ws;
+            }
+            else
+                return (Worksheet)XWorkbook.Worksheets[Name];
+        }
+        public void DeleteWorksheet(String Name)
+        {
+            if (WorksheetExist(Name))
+            {
+                XApplication.DisplayAlerts = false;
+                XWorkbook.Worksheets[Name].Delete();
+                XApplication.DisplayAlerts = true;
+            }
+        }
+        public bool WorksheetExist(String Name)
+        {
+            return XWorkbook.Worksheets.Keys().Contains(Name);
+        }
+    }
+
+    public static class ExcelExtension
+    {
+        public static IEnumerable<String> Keys(this Sheets sheets)
+        {
+            return sheets.Cast<Worksheet>().Select(x => x.Name);
+        }
+
+        public static IEnumerable<dynamic> GetEnumerable(this Sheets sheets)
+        {
+            return sheets.Cast<dynamic>();
+        }
+
+        public static Range GetRange(this Worksheet sheet, int Row, int Column)
+        {
+            return (Range)sheet.Cells[Row, Column];
+        }
+    }
+}
